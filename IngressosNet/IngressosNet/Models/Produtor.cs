1bc51bed0338943117e804259f1dffcd42607db4
danilo.silva <danilo.silva@msn.com>
2024-06-03 22:58:50 -0300

    Views de Atores e Produtores + Layout

--- IngressosNet/IngressosNet/Models/Produtor.cs
+++ IngressosNet/IngressosNet/Models/Produtor.cs
@@ -1,9 +1,16 @@
-﻿namespace IngressosNet.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IngressosNet.Models;
 
 public class Produtor : Entity
 {
+    [Display(Name = "Foto do Perfil")]
     public string FotoDoPerfil { get; set; } = string.Empty;
+
+    [Display(Name = "Nome Completo")]
     public string NomeCompleto { get; set; } = string.Empty;
+
+    [Display(Name = "Biografia")]
     public string Biografia { get; set; } = string.Empty;
 
     // Relacionamentos
